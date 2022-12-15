b2ba1416f33bc4230760cd1a0087c568a1c11a1b
Ashish Karhade <akarhade5@gmail.com>
2022-12-15 16:56:37 +0530

    login service created

---
+++ auth/server.py
@@ -0,0 +1,55 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+from dotenv import dotenv_values
+
+app = Flask(__name__)
+mysql = MySQL(app)
+
+# config
+envs = dotenv_values(".env")
+app.config["MYSQL_HOST"] = envs["MYSQL_HOST"]
+app.config["MYSQL_USER"] = envs["MYSQL_USER"]
+app.config["MYSQL_PASSWORD"] = envs["MYSQL_PASSWORD"]
+app.config["MYSQL_DB"] = envs["MYSQL_DB"]
+app.config["MYSQL_PORT"] = envs["MYSQL_PORT"]
+
+
+@app.route("/login")
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+
+    #check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username)
+    )
+    if res != 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, envs["JWT_SECRET"], True)
+    else:
+        return "invalid credentials", 401
+
+
+def createJWT(username, secret, is_admin):
+    return jwt.encode(
+        {
+            "username": username, 
+            "exp": datetime.datetime. now(tz=datetime.timezone.utc) + datetime.timedelta(days=1), #expiry in 24 hrs
+            "iat": datetime.datetime.now(),
+            "admin": is_admin
+        },
+        secret, 
+        algorithm="HS256"
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
